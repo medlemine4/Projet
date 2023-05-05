8afb9415e159a8bc3e206ef54aaefb8ce9748981
medlemine4 <130761003+medlemine4@users.noreply.github.com>
2023-05-05 18:43:28 +0000

    Add files via upload

---
+++ chois.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="chois.css">
+</head>
+
+<body>
+    <div class="contaner">
+        <br>
+
+        <div class="A">
+            <a href="enseignent.php"><button>Gerer les ensegnents</button></a>
+            <p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>
+            <a href="sujet.php"><button>Gerer les sujets</button></a>
+
+        </div>
+        <br> <br>
+        <br>
+        <br>
+        <br>
+
+        <div class="B">
+            <a href="#"><button>deconnection</button></a>
+        </div>
+        <br>
+
+    </div>
+</body>
+
+</html>
+
+<?php
+
+
+?>
